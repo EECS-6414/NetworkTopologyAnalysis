772a2142d082875c7759825d4c31dafa7dc7284d
Jaime Dantas <jaimejales@hotmail.com>
2021-02-19 02:31:37 -0300

    Added new stats

--- TNA.py
+++ TNA.py
@@ -6,6 +6,7 @@ import pandas as pd
 import matplotlib.lines as mlines
 from operator import itemgetter
 from fileNames import files
+from statistics import mean
 
 def TNAfunc():
     # Open input csv file, and create output files
@@ -56,11 +57,6 @@ def TNAfunc():
     G.add_nodes_from(listWithAllAppsHash.keys())
     G.add_nodes_from(authListHash.keys())
 
-    #for key, item in authListHash:
-
-
-
-
     # Add edge between every author and every app they commented on
     print("building network (2 min)...")
     for key, value in authListHash.items():
@@ -76,10 +72,37 @@ def TNAfunc():
     print("Network density:", density)
     topologyStatisticsFile.write("Network density: "+str(density))
     degree_dict = dict(G.degree(G.nodes()))
+    print("Extracting degree...")
+    degreeOfApp = {}
+    degreeOfAuthors = {}
+    for k, v in degree_dict.items():
+        if k in listWithAllAppsHash:
+            degreeOfApp[k] = v
+        else:
+            degreeOfAuthors[k] = v
+    degreeOfApp = sorted(degreeOfApp.items(), key=lambda x: x[1], reverse=True)
+    degreeOfAuthors = sorted(degreeOfAuthors.items(), key=lambda x: x[1], reverse=True)
+    appHighestDegree = str(list(degreeOfApp[0])[0]) + " - " + str(list(degreeOfApp[0])[1])
+    authorHighestDegree = str(list(degreeOfAuthors[0])[0]) + " - " + str(list(degreeOfAuthors[0])[1])
+    topologyStatisticsFile.write(appHighestDegree)
+    topologyStatisticsFile.write(authorHighestDegree)
+    print("average degree...")
+    count = 0
+    for v in degreeOfApp:
+        count += v[1]
+    topologyStatisticsFile.write("\nNode degree av. APP: "+str(count/len(degreeOfApp)))
+    count = 0
+    for v in degreeOfAuthors:
+        count += v[1]
+    topologyStatisticsFile.write("\nNode degree av. Author: "+str(count/len(degreeOfAuthors)))
+    topologyStatisticsFile.write("\nTop 10 Authors:\n")
+    for v in range(10):
+        topologyStatisticsFile.write(str(degreeOfAuthors[v])+"\n")
+
     nx.set_node_attributes(G, degree_dict, 'degree')
     sorted_degree = sorted(degree_dict.items(), key=itemgetter(1), reverse=True)
     print("Top 5 nodes by degree:")
-    topologyStatisticsFile.write("top 10 Apps:")
+    topologyStatisticsFile.write("\nTop 10 Apps:\n")
     for d in sorted_degree[:10]:
         print(d)
         topologyStatisticsFile.write(str(d)+"\n")
@@ -91,30 +114,30 @@ def TNAfunc():
     print("Weights assigned")
 
     print("Map to layout (takes time)...")
-    # Map to layout
-    pos = nx.spring_layout(G, k=0.25, iterations=70, scale=10)
-    print("size of graph: "+str(G.size()))
-    plt.figure(figsize=(20, 20))
-
-    print("Draw network..")
-    # Draw network
-    nx.draw_networkx_nodes(G, pos=pos, node_color="red", nodelist=listWithAllAppsHash.keys(), node_size=appWeight)
-    nx.draw_networkx_nodes(G, pos=pos, node_color="blue", nodelist=authListHash.keys(), node_size=authWeight)
-    nx.draw_networkx_edges(G, pos=pos)
-
-    print("Add legend")
-    # Add legend
-    p = mlines.Line2D([],[],label="Author",color="blue")
-    a = mlines.Line2D([],[],label="Application",color="red")
-    plt.legend(handles=[p,a])
-
-    try:
-        print("Print graph")
-        # Print graph list to output, and output image
-        graphDictionaryFile.write(str(G.edges))
-        plt.savefig("TopologicalAnalysis.jpg", dpi=40, pil_kwargs={'quality': 50})
-    except:
-        print("Reached exception when printing JPG")
+    # # Map to layout
+    # pos = nx.spring_layout(G, k=0.25, iterations=70, scale=10)
+    # print("size of graph: "+str(G.size()))
+    # plt.figure(figsize=(20, 20))
+    #
+    # print("Draw network..")
+    # # Draw network
+    # nx.draw_networkx_nodes(G, pos=pos, node_color="red", nodelist=listWithAllAppsHash.keys(), node_size=appWeight)
+    # nx.draw_networkx_nodes(G, pos=pos, node_color="blue", nodelist=authListHash.keys(), node_size=authWeight)
+    # nx.draw_networkx_edges(G, pos=pos)
+    #
+    # print("Add legend")
+    # # Add legend
+    # p = mlines.Line2D([],[],label="Author",color="blue")
+    # a = mlines.Line2D([],[],label="Application",color="red")
+    # plt.legend(handles=[p,a])
+    #
+    # try:
+    #     print("Print graph")
+    #     # Print graph list to output, and output image
+    #     graphDictionaryFile.write(str(G.edges))
+    #     plt.savefig("TopologicalAnalysis.jpg", dpi=40, pil_kwargs={'quality': 50})
+    # except:
+    #     print("Reached exception when printing JPG")
 
 
     # Return graph to call function
